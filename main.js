e46339747f46225833051813576c329ee4149c79
SalmanDeveloperz <151463017+SalmanDeveloperz@users.noreply.github.com>
2023-12-06 15:33:03 +0500

    Salman's Website
    My first portfolio website

---
+++ main.js
@@ -0,0 +1,149 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction() {
+    let menuBtn = document.getElementById("myNavMenu");
+
+    if (menuBtn.className === "nav-menu") {
+        menuBtn.className += " responsive";
+    } else {
+        menuBtn.className = "nav-menu";
+    }
+}
+
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+window.onscroll = function () { headerShadow() };
+
+function headerShadow() {
+    const navHeader = document.getElementById("header");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+
+        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+
+    } else {
+
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+
+    }
+}
+
+
+/* ----- TYPING EFFECT ----- */
+let typingEffect = new Typed(".typedText", {
+    strings: ["Software Engineer", "Programmer", "Data Architect", "Game Developer", "Algorithmic Analyst"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000
+})
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card', {})
+sr.reveal('.featured-name', { delay: 100 })
+sr.reveal('.featured-text-info', { delay: 200 })
+sr.reveal('.featured-text-btn', { delay: 200 })
+sr.reveal('.social_icons', { delay: 200 })
+sr.reveal('.featured-image', { delay: 300 })
+
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box', { interval: 200 })
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header', {})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+srLeft.reveal('.about-info', { delay: 100 })
+srLeft.reveal('.contact-info', { delay: 100 })
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+    origin: 'right',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+srRight.reveal('.skills-box', { delay: 100 })
+srRight.reveal('.form-control', { delay: 100 })
+
+
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+        } else {
+
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+function downloadCV() {
+    // Replace 'M.Salman%20Resume.pdf' with the actual name of your CV file
+    var cvUrl = 'M.Salman%20Resume.pdf';
+
+    // Create a temporary link element
+    var link = document.createElement('a');
+
+    // Set the href attribute with the file URL
+    link.href = cvUrl;
+
+    // Set the download attribute with the desired file name
+    link.download = 'M.Salman%20Resume.pdf';
+
+    // Append the link to the document
+    document.body.appendChild(link);
+
+    // Trigger a click event on the link to start the download
+    link.click();
+
+    // Remove the link from the document
+    document.body.removeChild(link);
+}
+
+function scrollToContact() {
+    const contactSection = document.getElementById("contact");
+
+    // Scroll smoothly to the contact section
+    contactSection.scrollIntoView({ behavior: "smooth" });
+}
+
+
